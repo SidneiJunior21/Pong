dea9edd17385ff8ca19b9b55b6701f382db59c42
redbdb <gustavoasamaral@gmail.com>
2025-02-06 01:55:11 -0300

    player não consegue sair do mapa, revisar mais tarde talvez

--- Pong/Assets/Scripts/Jogadores.cs
+++ Pong/Assets/Scripts/Jogadores.cs
@@ -4,6 +4,9 @@ public class Jogadores : MonoBehaviour
 {
     public float velocidadeDoJogador;
     public bool Jogador1;
+
+    public float BordaInferior;
+    public float BordaSuperior;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,24 +23,24 @@ public class Jogadores : MonoBehaviour
         }else{moverJogador2();}
         
     }
-    private void moverJogador1()
-    {
-        if(Input.GetKey(KeyCode.W))
+    private void moverJogador1()//verificar se não existe outro jeito de limitar o player mais tarde
+    {//W e S para mover, só move se estiver dentro da área do jogo
+        if(Input.GetKey(KeyCode.W) && transform.position.y <= BordaSuperior)
         {
             transform.Translate(Vector2.up * velocidadeDoJogador * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.S))
+        if(Input.GetKey(KeyCode.S) && transform.position.y >= BordaInferior)
         {
             transform.Translate(Vector2.down * velocidadeDoJogador * Time.deltaTime);
         }
     }
     private void moverJogador2()
-    {
-        if(Input.GetKey(KeyCode.UpArrow))
+    {//setas para cima e baixo para mover, só move se estiver dentro da área do jogo
+        if(Input.GetKey(KeyCode.UpArrow) && transform.position.y <= BordaSuperior)
         {
             transform.Translate(Vector2.up * velocidadeDoJogador * Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.DownArrow))
+        if(Input.GetKey(KeyCode.DownArrow) && transform.position.y >= BordaInferior)
         {
             transform.Translate(Vector2.down * velocidadeDoJogador * Time.deltaTime);
         }
