975bb061a7af98fd8ad31e2fe8f6056bba6b9429
redbdb <gustavoasamaral@gmail.com>
2025-02-14 09:46:56 -0300

    pontuação aparecendo na tela

--- Pong/Assets/Scripts/SistemaPontuação.cs
+++ Pong/Assets/Scripts/SistemaPontuação.cs
@@ -1,15 +1,30 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SistemaPontuação : MonoBehaviour
 {//gerencia a pontuação de cada um dos jogadores, uma funçõa para aumentar a pontuação de cada um
     public int pontuacao1; 
     public int pontuacao2;
+
+    public Text txtPont;
+
+    void Start(){
+        pontuacao1 = 0;
+        pontuacao2 = 0;
+        attPont();
+    }
   
     public void pontuarP1(){
         pontuacao1 += 1;
+        attPont();
     }
 
     public void pontuarP2(){
         pontuacao2 += 1;
+        attPont();
+    }
+
+    public void attPont(){
+        txtPont.text = pontuacao1 + " : " + pontuacao2;
     }
 }
